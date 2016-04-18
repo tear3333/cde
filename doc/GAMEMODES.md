505cb0cfc47ca53e56773c3f1c8cadcd2173b64d
tear3333 <1984869533@qq.com>
2016-04-18 17:37:54 +0800

    Added files via upload

---
+++ doc/GAMEMODES.md
@@ -0,0 +1,13 @@
+# Custom Gamemodes
+
+Id   | Name
+-----|--------------
+0    | Free For All
+1    | Teams
+2    | Experimental
+10   | Tournament
+11   | Hunger Games
+12   | Zombie Mode
+13	 | Zombie Team
+14	 | Leap
+20   | Rainbow FFA
---
+++ server/ai/BotLoader.js
@@ -0,0 +1,51 @@
+// Project imports
+var BotPlayer = require('./BotPlayer');
+var FakeSocket = require('./FakeSocket');
+var PacketHandler = require('../PacketHandler');
+
+function BotLoader(gameServer) {
+    this.gameServer = gameServer;
+    this.loadNames();
+}
+
+module.exports = BotLoader;
+
+BotLoader.prototype.getName = function() {
+    var name = "";
+
+    // Picks a random name for the bot
+    if (this.randomNames.length > 0) {
+        var index = Math.floor(Math.random() * this.randomNames.length);
+        name = this.randomNames[index];
+        this.randomNames.splice(index,1);
+    } else {
+        name = "bot" + ++this.nameIndex;
+    }
+
+    return name;
+};
+
+BotLoader.prototype.loadNames = function() {
+    this.randomNames = [];
+		var fs = require("fs"); // Import the util library
+        var path = require("path");
+    // Load names
+    this.randomNames = fs.readFileSync(path.join(__dirname, '../', 'botnames.txt'), "utf8").split(/[\r\n]+/).filter(function(x) {
+            return x != ''; // filter empty names
+        });
+	console.log(this.randomNames);
+    this.nameIndex = 0;
+};
+
+BotLoader.prototype.addBot = function() {
+    var s = new FakeSocket(this.gameServer);
+    s.playerTracker = new BotPlayer(this.gameServer, s);
+    s.packetHandler = new PacketHandler(this.gameServer, s);
+
+    // Add to client list
+    this.gameServer.clients.push(s);
+
+    // Add to world
+    s.packetHandler.setNickname(this.getName());
+};
+
